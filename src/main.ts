3166dfb0c1d1fd837f51db1272829516edf36846
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2024-11-22 10:35:22 -0500

    feat(chore): se finaliza la configuracion en modo desarrollo de docker

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  logger.log('Application listening on port 3000');
 }
 bootstrap();
